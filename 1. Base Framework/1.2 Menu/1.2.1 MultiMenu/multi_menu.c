bbde997c9b474f0a38134ff3fcf0d33c3c852fc9
zjcszn <zjcszn@gmail.com>
2022-12-17 12:58:37 +0800

    update multi menu

--- 1. Base Framework/1.2 Menu/1.2.1 MultiMenu/multi_menu.c
+++ 1. Base Framework/1.2 Menu/1.2.1 MultiMenu/multi_menu.c
@@ -1,24 +1,55 @@
+/***********************************************************************************
+ * 
+ * author: zjcszn
+ * date: 2022/12/17
+ * version: 1.0
+ * 
+ * MIT License
+ * 
+ * Copyright (c) 2022 zjcszn
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ * 
+ * ********************************************************************************/
+
 #include "multi_menu.h"
 #include <assert.h>
 #include <string.h>
 
-/**************************** Macro Definition ***************************/
+/******************************** Macro Definition ********************************/
 
-#define MENU_DISP_MAX_ITEMS       (2U)      // max number of display items
-#define MENU_TREE_DEPTH           (3U)      // max level  of menu tree
+#define MENU_DISP_MAX_ITEMS       (2U)          // max number of display items
+#define MENU_TREE_DEPTH           (3U)          // max level  of menu tree
 
-#define menu_assert(exp)          assert(exp)
-/**************************** Private Variable ***************************/
+#define menu_assert(exp)          assert(exp)   // menu assert macro
 
-static MenuList menu_list;                  // pointer of menu list
-static uint32_t menu_nums;                  // number  of menu node
+/******************************** Private Variable ********************************/
 
-static MenuConfig  menu_config;             // config  of menu display
-static uint32_t    refresh_flag;            // menu ui refresh flag
+static MenuList     menu_list;                  // pointer of menu list
+static uint32_t     menu_nums;                  // number  of menu node
+
+static MenuConfig   menu_config;                // config  of menu display
+static uint32_t     refresh_flag;               // menu ui refresh flag
 
 static MenuUI_Handler  menu_ui_handler = NULL;
 
-/*************************** Menu Config Stack ***************************/
+/******************************* Menu Config Stack ********************************/
 
 #define MENU_CFG_STACK_SIZE       (MENU_TREE_DEPTH - 1)
 
@@ -27,7 +58,7 @@ static struct {
   __IO uint8_t sp;
 } menu_cfg_stack = {0};
 
-/*************************** Menu Message Queue **************************/
+/******************************* Menu Message Queue *******************************/
 
 #define MENU_MSG_FIFO_SIZE        (4U)
 #define MENU_MSG_FIFO_MASK        (MENU_MSG_FIFO_SIZE - 1)
@@ -38,23 +69,42 @@ static struct {
   __IO uint8_t w;
 }menu_msg_fifo = {0};
 
-static inline void fifo_reset(void) {
+/**
+ * @brief clear message queue
+ * 
+ */
+static inline void menu_msg_clear(void) {
   menu_msg_fifo.r = menu_msg_fifo.w = 0;
 }
 
-static inline uint8_t fifo_used(void) {
+/**
+ * @brief get count of message in the queue
+ * 
+ * @return uint8_t message count
+ */
+static inline uint8_t menu_msg_count(void) {
   return (menu_msg_fifo.w - menu_msg_fifo.r);
 }
 
-static inline int is_fifo_empty(void) {
+/**
+ * @brief check if queue is empty
+ * 
+ * @return int empty on 1 | not empty on 0
+ */
+static inline int check_menu_msg_empty(void) {
   return (menu_msg_fifo.w == menu_msg_fifo.r);
 }
 
-static inline int is_fifo_full(void) {
-  return (fifo_used() == MENU_MSG_FIFO_SIZE);
+/**
+ * @brief check if queue is full
+ * 
+ * @return int full on 1 | not full on 0
+ */
+static inline int check_menu_msg_full(void) {
+  return (menu_msg_count() == MENU_MSG_FIFO_SIZE);
 }
 
-/********************* Private Function Declaration **********************/
+/************************** Private Function Declaration **************************/
 
 static void menu_display_init(MenuNode *_cur_page, uint32_t _max_disp);
 static void menu_msg_handler(void);
@@ -63,7 +113,7 @@ static int  menu_config_push(void);
 static int  menu_config_pop(void);
 static MenuNode *find_menu_node(uint32_t target_id);
 
-/************************* Function Definition ***************************/
+/****************************** Function Definition *******************************/
 
 
 
@@ -165,14 +215,19 @@ static void menu_msg_handler(void) {
         }
       }
       else {
-        menu_config_push();
-        menu_display_init(menu_config.cur_item, MENU_DISP_MAX_ITEMS);
+        if (menu_config_push()) {
+          menu_display_init(menu_config.cur_item, MENU_DISP_MAX_ITEMS);
+        }
+        else {
+          menu_assert(0);     // can not arrive here
+        }
       }
       break;
 
     case MENU_MSG_ESC:
-      refresh_flag = MENU_REFRESH_FULL;
-      menu_config_pop();
+      if (menu_config_pop()) {
+        refresh_flag = MENU_REFRESH_FULL;
+      }
       break;
   }
 }
@@ -186,7 +241,7 @@ static void menu_msg_handler(void) {
  */
 static int menu_msg_dequeue(uint8_t *_menu_msg) {
   menu_assert(_menu_msg);
-  if (is_fifo_empty()) return 0;
+  if (check_menu_msg_empty()) return 0;
   *_menu_msg = menu_msg_fifo.msg_buf[menu_msg_fifo.r & MENU_MSG_FIFO_MASK];
   menu_msg_fifo.r++;
   return 1;
@@ -199,7 +254,7 @@ static int menu_msg_dequeue(uint8_t *_menu_msg) {
  * @return int success on 1 | failed on 0
  */
 int menu_msg_enqueue(uint8_t _menu_msg) {
-  if (is_fifo_full()) return 0;
+  if (check_menu_msg_full()) return 0;
   menu_msg_fifo.msg_buf[menu_msg_fifo.w & MENU_MSG_FIFO_MASK] = _menu_msg;
   menu_msg_fifo.w++;
   return 1;
@@ -236,6 +291,7 @@ static int menu_config_pop(void) {
  * @return MenuNode *pointer of menu node
  */
 static MenuNode *find_menu_node(uint32_t target_id) {
+  menu_assert(menu_list);
   for(int i = 1; i < menu_nums; i++) {
     if (menu_list[i].id == target_id) return &menu_list[i];
   }
--- 1. Base Framework/1.2 Menu/1.2.1 MultiMenu/multi_menu.h
+++ 1. Base Framework/1.2 Menu/1.2.1 MultiMenu/multi_menu.h
@@ -1,3 +1,33 @@
+/***********************************************************************************
+ * 
+ * author: zjcszn
+ * date: 2022/12/17
+ * version: 1.0
+ * 
+ * MIT License
+ * 
+ * Copyright (c) 2022 zjcszn
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ * 
+ * ********************************************************************************/
+
 #ifndef __MULTI_MENU_H__
 #define __MULTI_MENU_H__
 
@@ -7,7 +37,7 @@
 extern "C" {
 #endif
 
-/*************************** Macro Definition ***************************/
+/******************************** Macro Definition ********************************/
 
 #ifndef __IO
 #define __IO volatile
@@ -17,7 +47,7 @@ extern "C" {
 #define NULL ((void*)0U)
 #endif
 
-/*************************** Type Declaration ***************************/
+/******************************** Type Definition *********************************/
 
 typedef struct menu_node    MenuNode;
 typedef struct menu_node   *MenuList;
@@ -56,7 +86,7 @@ struct menu_config{
 //uint32_t  max_disp;  // max number of item in the display list       
 };
 
-/********************* Extern Function Declaration **********************/
+/************************** Extern Function Declaration ***************************/
 
 extern void menu_init(MenuList _menu_list, uint32_t _menu_nums, MenuUI_Handler *ui_callback);
 extern void menu_loop(void);
