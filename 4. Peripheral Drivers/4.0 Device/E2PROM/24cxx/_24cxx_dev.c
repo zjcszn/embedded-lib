23701d612660a9edc1d3ae8d4d50eff958ca2f23
zjcszn <zjcszn@gmail.com>
2023-01-10 23:45:01 +0800

    add 24cxx driver

---
+++ 4. Peripheral Drivers/4.0 Device/E2PROM/24cxx/_24cxx_dev.c
@@ -0,0 +1,335 @@
+/*
+ * File      : _24cxx_dev.c
+ * 
+ * This file is part of eeprom device for 24cxx,such as at24c02/at24c16.
+ * COPYRIGHT (C) 2016
+ *
+ * Change Logs:
+ * Date           Author       Version			Notes
+ * 2018-09-01     Acuity      	1.00		first version.
+ * 2020-04-20     Acuity      	1.01		i2c总线提取出去.
+ *
+ * Note:
+ * 
+ */
+ 
+#include "_24cxx_dev.h"	
+
+ /**
+ * @brief  get page size
+ * @param  model:eeprom model
+ * @retval page size
+ */  
+static uint16_t get_eeprom_pagesize(_24_model_t model) 
+{
+	switch(model)
+	{
+    case _24C01_E:
+		case _24C02_E:
+		  return 8;
+		  
+		case _24C04_E:
+		case _24C08_E:
+		case _24C16_E:
+		  return 16;
+		  
+		case _24C32_E:
+		case _24C64_E:
+      return 32;
+        
+		case _24C128_E:
+		case _24C256_E:
+      return 64;
+
+		case _24C512_E:
+      return 128;
+
+		case _24C1024_E:
+		  return 256;
+		  
+		default:
+	    return 0;
+	}
+}
+
+  /**
+ * @brief  get chip size
+ * @param  model:eeprom model
+ * @retval chip size,uinit:byte 
+ */  
+static uint32_t get_eeprom_chipsize(_24_model_t model) 
+{
+	switch(model)
+	{
+	  case _24C01_E:
+		  return 128u;
+		case _24C02_E:
+		  return 256u;
+		case _24C04_E:
+		  return 512u;
+		case _24C08_E:
+		  return 1024u;
+		case _24C16_E:
+		  return 2048u;
+		case _24C32_E:
+		  return 4096u;
+		case _24C64_E:
+		  return 8192u;
+		case _24C128_E:
+		  return 16384u;
+		case _24C256_E:
+		  return 32768u;
+		case _24C512_E:
+		  return 65536u;
+		case _24C1024_E:
+		  return 131072u;
+		default:
+	    return 0;
+	}
+}
+
+/**
+ * @brief  write one page. 
+ * @param  pdev pointer to the eeprom device struct.
+ * @param  addr the address of write to.
+ * @param  pbuf the data to write.
+ * @param  size number of bytes to write..
+ * @retval return 0 if 0k,anything else is considered an error.
+ */
+static int16_t _24cxx_write_page(_24cxx_dev_t *pdev, uint32_t addr, uint8_t *pbuf, uint32_t size)
+{
+	uint8_t	 buf[2];
+	uint8_t	 buf_size = 0;
+	uint8_t  slave_addr = 0;
+	uint16_t ee_page_size = 0;
+	int16_t	ret = 0;
+	
+	if (pdev == 0)
+	{
+		return _24CXX_ERR_DEV_NONE;
+	}
+
+	ee_page_size = get_eeprom_pagesize(pdev->model);	
+	if (((addr % ee_page_size) + size) > ee_page_size) /*the over flow of page size*/
+	{
+		return _24CXX_ERR_PAGE_SIZE;
+	}
+	if (pdev->model > _24C16_E)
+	{/*24c32-24c1024*/
+		slave_addr = pdev->slave_addr;
+		buf[0] = (addr >>8)& 0xff;
+		buf[1] = addr & 0xff;
+		buf_size = 2;
+	}
+	else
+	{/*24c01-24c16*/
+		slave_addr = pdev->slave_addr | (addr>>8);
+		buf[0] = addr & 0xff;
+		buf_size = 1;
+	}
+	
+	if (pdev->wp)		/*release write protect*/
+	{
+		pdev->wp(0);
+	}
+	
+	ret = pdev->i2c_send_then_send(slave_addr, buf, buf_size, pbuf, size);
+	
+	if (pdev->wp)
+	{
+		pdev->wp(1);	/*write protect*/
+	}
+	
+	return ret;
+}
+
+/**
+ * @brief  read from the eeprom.
+ * @param  pdev pointer to the eeprom device struct.
+ * @param  addr the address to read from.
+ * @param  pbuf the data to write.
+ * @param  size number of bytes to read.
+ * @retval return 0 if 0k,anything else is considered an error.
+ */
+static int16_t _24cxx_write_continue(_24cxx_dev_t *pdev, uint32_t addr, uint8_t *pbuf, uint32_t size)
+{
+  uint8_t offset = 0;
+	uint8_t write_size = 0;
+	uint8_t	buf[2];
+	uint8_t slave_addr = 0;
+	int16_t	ret = 0;
+	
+	if (pdev == 0)
+	{
+		return _24CXX_ERR_DEV_NONE;
+	}
+	
+	if (pdev->wp)		/* release write protect */
+	{
+		pdev->wp(0);
+	}
+	
+	if (pdev->model > _24C16_E)
+	{/* 24c32-24c1024 */
+		slave_addr = pdev->slave_addr;
+		buf[0] = (addr >>8)& 0xff;
+		buf[1] = addr & 0xff;
+		ret = pdev->i2c_send_then_send(slave_addr, buf, 2, pbuf, size);
+	}
+	else
+	{/* 24c01-24c16 */
+		while (size > 0)
+		{
+			offset = 0xff - (addr % 0xff);
+			write_size = size > offset ? offset : size;
+			slave_addr = pdev->slave_addr | (addr>>8);
+			buf[0] = addr & 0xff;
+		
+			ret = pdev->i2c_send_then_send(slave_addr, buf, 1, pbuf, write_size);
+			if (ret != _24CXX_OK)
+			{
+				break;
+			}
+			size = size - write_size;
+			if (size > 0)
+			{
+				pbuf = pbuf + write_size;
+				addr = addr + write_size;
+			}
+		}
+	}
+	
+	if (pdev->wp)
+	{
+		pdev->wp(1);	/* write protect */
+	}
+	
+	return ret;
+}
+
+/**
+ * @brief  write data to eeprom.
+ * @param  pdev pointer to the eeprom device struct.
+ * @param  addr the address of write to.
+ * @param  pbuf the data to write.
+ * @param  size number of bytes to write..
+ * @retval return 0 if 0k,anything else is considered an error.
+ */
+int16_t _24cxx_write(_24cxx_dev_t *pdev, uint32_t addr, uint8_t *pbuf, uint32_t size)
+{
+	uint8_t write_len,page_offset;
+	int8_t error = 0;
+	uint16_t ee_page_size = 0;
+	
+	if (pdev == 0)
+	{ 	
+		return _24CXX_ERR_DEV_NONE;
+	}
+	if ((addr+size) > get_eeprom_chipsize(pdev->model))	/*the over flow of chip size*/
+	{
+		return _24CXX_ERR_CHIP_SIZE;
+	}
+	
+	if (0 == pdev->page_write_delay)
+	{/* FRAM */
+		error = _24cxx_write_continue(pdev, addr, pbuf, size);
+	}
+	else
+	{/* EEPROM */
+		ee_page_size = get_eeprom_pagesize(pdev->model);
+		while (size > 0)
+		{
+			page_offset = ee_page_size - (addr % ee_page_size);
+			write_len   = size > page_offset ? page_offset : size;
+			error = _24cxx_write_page(pdev,addr,pbuf,write_len);	
+			if(error) break;
+			size = size - write_len;
+			if (size > 0)
+			{
+				pbuf = pbuf + write_len;
+				addr = addr + write_len;
+			}
+			if (pdev->page_write_delay)
+			{
+				pdev->page_write_delay();		/*eeprom need wait*/
+			}
+		}
+	}
+	return error;
+}
+
+/**
+ * @brief  read from the eeprom.
+ * @param  pdev pointer to the eeprom device struct.
+ * @param  addr the address to read from.
+ * @param  pbuf where to put read data.
+ * @param  size number of bytes to read.
+ * @retval return 0 if 0k,anything else is considered an error.
+ */
+int16_t _24cxx_read(_24cxx_dev_t *pdev,uint32_t addr, uint8_t *pbuf, uint32_t size)
+{  
+	uint8_t	buf[2];
+	uint8_t buf_size = 0;
+	uint8_t slave_addr;
+	int16_t ret = 0;
+	
+	if (pdev == 0)
+	{
+		return _24CXX_ERR_DEV_NONE;
+	}
+	if ((addr+size) > get_eeprom_chipsize(pdev->model))	 
+	{
+		return _24CXX_ERR_CHIP_SIZE;
+	}
+	if (pdev->model > _24C16_E)
+	{
+		slave_addr = pdev->slave_addr;
+		buf[0] 	   = (addr >>8)& 0xff;
+		buf[1] 	   = addr & 0xff;
+		buf_size   = 2;
+	}
+	else
+	{
+		slave_addr = pdev->slave_addr | (addr>>8);
+		buf[0] = addr & 0xff;
+		buf_size = 1;
+	}
+	
+	ret = pdev->i2c_send_then_recv(slave_addr, buf, buf_size, pbuf, size);
+	
+	return ret;
+}
+
+/**
+ * @brief  erase the eeprom.
+ * @param  pdev pointer to the eeprom device struct.
+ * @param  addr the address to erase from.
+ * @param  data padding data.
+ * @param  size number of bytes to erase.
+ * @retval return 0 if ok,anything else is considered an error.
+ */
+int16_t _24cxx_erase(_24cxx_dev_t *pdev,uint32_t addr, uint8_t data, uint32_t size)
+{
+	int8_t error = 0;
+	uint16_t i;
+	
+	if (pdev == 0)
+	{
+		return _24CXX_ERR_DEV_NONE;
+	}
+	if ((addr+size) > get_eeprom_chipsize(pdev->model))	 
+	{
+		return _24CXX_ERR_CHIP_SIZE;
+	}
+	for (i = 0;i < size;i++)
+	{
+		error = _24cxx_write_page(pdev,addr,&data,size);
+		if(error)
+		{
+			break;
+		}
+	}
+				
+	return error;
+}
+
---
+++ 4. Peripheral Drivers/4.0 Device/E2PROM/24cxx/_24cxx_dev.h
@@ -0,0 +1,52 @@
+#ifndef _24Cxx_DEV_H_
+#define _24Cxx_DEV_H_
+
+#include <stdint.h>
+
+/*operation state*/
+enum _24CXX_ERROR
+{
+	_24CXX_OK 						= 0,	
+	_24CXX_ERR_I2C_WR 		= 1,
+	_24CXX_ERR_DEV_NONE 	= 2,
+	_24CXX_ERR_PAGE_WRITE	= 3,
+	_24CXX_ERR_PAGE_SIZE	= 4,
+	_24CXX_ERR_CHIP_SIZE	= 5,
+};
+
+/*eeprom model*/
+typedef enum 
+{
+  _24C01_E = 1,
+	_24C02_E,
+	_24C04_E,
+	_24C08_E,
+	_24C16_E,
+	_24C32_E,
+	_24C64_E,
+	_24C128_E,
+	_24C256_E,
+	_24C512_E,
+	_24C1024_E,
+}_24_model_t;
+
+/*24cxx eeprom devcie struct*/
+typedef struct 
+{
+	int (*i2c_send_then_recv)(uint8_t slave_addr, const void *send_buff, 	/* i2c bus fun */
+				uint32_t send_size, void *recv_buff, uint32_t recv_size);
+	int (*i2c_send_then_send)(uint8_t slave_addr, const void *send_buff1,	/* i2c bus fun */
+				uint32_t send_size1,const void *send_buff2, uint32_t send_size2);
+	uint8_t			slave_addr;					/*eeprom i2c addr*/
+	_24_model_t	model;							/*eeprom model*/
+	void(*wp)(uint8_t ctrl);				/*protect of write function*/
+	void(*page_write_delay)(void);	/*there is a delay in continuous writin for EEPROM,FRAM not need*/
+	
+}_24cxx_dev_t;
+
+/*extern function*/
+extern int16_t _24cxx_read(_24cxx_dev_t *pdev,uint32_t addr, uint8_t *pbuf, uint32_t size);
+extern int16_t _24cxx_write(_24cxx_dev_t *pdev,uint32_t addr,uint8_t *pbuf,uint32_t size);
+extern int16_t _24cxx_erase(_24cxx_dev_t *pdev,uint32_t addr,uint8_t data, uint32_t size);
+
+#endif
